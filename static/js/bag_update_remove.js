084d3c15564c46ede63a25dbcf9924d2fb07de6c
Richard Messenger <twmd95gh@students.codeinstitute.net>
2024-07-09 16:54:21 +0000

    Updated sutotal function.

--- static/js/bag_update_remove.js
+++ static/js/bag_update_remove.js
@@ -6,11 +6,11 @@ $('.update-link').click(function(e) {
 
 // Remove item and reload on click
 $('.remove-item').click(function(e) {
-    var csrfToken = "{{ csrf_token }}";
+    var csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
     var itemId = $(this).attr('id').split('remove_')[1];
-    var size = $(this).data('size');
-    var url = `/bag/remove/${itemId}`;
-    var data = {'csrfmiddlewaretoken': csrfToken, 'size': size};
+    var size = $(this).data('product_size');
+    var url = `/bag/remove/${itemId}/`;
+    var data = {'csrfmiddlewaretoken': csrfToken, 'product_size': size};
 
     $.post(url, data)
      .done(function() {
