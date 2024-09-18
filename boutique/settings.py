51b98be84f9e890c6c75add60571bf35436eb47f
Richard Messenger <twmd95gh@students.codeinstitute.net>
2024-09-18 16:24:03 +0000

    Updated settings

--- boutique/settings.py
+++ boutique/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qbx7ahpj2v2qu7r9q0dliyk#sslb(-x#fa+w(jau@nkeix(ns5'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = ['richies-shop-app.herokuapp.com', 'localhost']
 
