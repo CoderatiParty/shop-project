f6f02ecd83e648d0b11cda9eddf52056175e216b
Richard Messenger <twmd95gh@students.codeinstitute.net>
2024-09-18 16:58:09 +0000

    Procfile update

--- boutique/settings.py
+++ boutique/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = ['https://richies-shop-app-d7abb52ef875.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['https://richies-shop-app.herokuapp.com/', 'localhost']
 
 
 # Application definition
