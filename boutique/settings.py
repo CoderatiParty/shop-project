c27071b7166e64960c645b9ef1dc2458048b4e18
Richard Messenger <twmd95gh@students.codeinstitute.net>
2024-09-19 13:59:41 +0000

    aws

--- boutique/settings.py
+++ boutique/settings.py
@@ -179,10 +179,16 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
-# MEDIA_URL = '/media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'richies-shop-project-bucket'
     AWS_S3_REGION_NAME = 'eu-north-1'
